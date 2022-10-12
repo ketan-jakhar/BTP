9668252f894c5876dd96e6883c494fd47fe5aa79
Ketan Jakhar <70067813+ketan-jakhar@users.noreply.github.com>
2022-10-13 02:50:04 +0530

    [fix]: linting, prettier, refactoring, new packages added, typescript support updated

---
+++ server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
